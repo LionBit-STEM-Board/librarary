0260a1c38b757cdaeebcd88d847dd4556c75dfe6
Kavinda Kotuwegedara <kotuwegedara@gmail.com>
2023-09-03 07:50:33 +0530

    library add

---
+++ examples/TouchSensor/TouchSensor.ino
@@ -0,0 +1,19 @@
+
+volatile int capacitiveValue=100;
+volatile int threshold=10;
+
+void setup() {
+   
+    Serial.begin(115200);
+    delay(1000); // Delay to launch the serial monitor
+    Serial.println("ESP32 Touch Demo");
+}
+
+void loop() {
+    capacitiveValue = touchRead(A4);
+    if(capacitiveValue < threshold ){
+        Serial.println("Wire touched");
+        }
+         Serial.println("Wire Not");
+    delay(500);
+}
